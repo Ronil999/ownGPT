ac75f624761667ac77302fa081f4f76d1ec481ab
Ronil999 <104007767+Ronil999@users.noreply.github.com>
2023-06-05 14:15:43 +0530

    Update config.js

--- server/db/config.js
+++ server/db/config.js
@@ -1,2 +1,3 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/ownGPT");
+// mongoose.connect("mongodb://127.0.0.1:27017/ownGPT");
+mongoose.connect(process.env.MONGOURI);
