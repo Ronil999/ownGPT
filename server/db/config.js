38215254ada7f3277e46b953b4f12b8d1e5fe55e
Ronil999 <104007767+Ronil999@users.noreply.github.com>
2023-06-06 14:13:32 +0530

    Update config.js

--- server/db/config.js
+++ server/db/config.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 // mongoose.connect("mongodb://127.0.0.1:27017/ownGPT");
-// mongoose.connect(process.env.MONGOURI);
+mongoose.connect(process.env.MONGOURI);
 
-mongoose.connect("mongodb+srv://ronilcoder999:wfO4LmraAjvrqDMG@owngpt.wx89abe.mongodb.net/?retryWrites=true&w=majority");
+// mongoose.connect("mongodb+srv://ronilcoder999:wfO4LmraAjvrqDMG@owngpt.wx89abe.mongodb.net/?retryWrites=true&w=majority");
