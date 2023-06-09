b8e49788030a741632d8c4d7985c088a94010859
Ronil999 <104007767+Ronil999@users.noreply.github.com>
2023-06-09 15:22:38 +0530

    for store image history

--- server/db/History.js
+++ server/db/History.js
@@ -1,12 +1,24 @@
 const mongoose = require('mongoose');
 
+// const historySchema = new mongoose.Schema({
+//     userId:String,
+//     userInput:String,
+//     aiOutput:String,
+//     date: {
+//         type: Date, default: Date.now,
+//     }
+// });
+
 const historySchema = new mongoose.Schema({
-    userId:String,
-    userInput:String,
-    aiOutput:String,
+    userId: String,
+    app: String,
+    userInput: String,
+    aiOutput: mongoose.Schema.Types.Mixed,
     date: {
-        type: Date, default: Date.now,
-    }
+        type: Date,
+        default: Date.now,
+    },
 });
+
 
 module.exports = mongoose.model("history",historySchema);
