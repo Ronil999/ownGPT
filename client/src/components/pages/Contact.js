bc36b126af461ceecf5a5b0ef2223c82379400b2
Ronil999 <104007767+Ronil999@users.noreply.github.com>
2023-07-26 22:45:01 +0530

    Contact us page update

--- client/src/components/pages/Contact.js
+++ client/src/components/pages/Contact.js
@@ -1,56 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Contact.css'
 
 const Contact = () => {
+
+    const [name, setName] = useState("");
+    const [subject, setSubject] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleClick = async () => {
+
+        if (!name || !subject || !email || !phone || !message) {
+            alert('Please fill in all required fields');
+            return;
+        }
+
+        else {
+            let result = await fetch('http://localhost:8000/contact', {
+                method: 'POST',
+
+                body: JSON.stringify({ name, subject, email, phone, message }),
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+            result = await result.json();
+            if (result.success) {
+                alert("Thank you for contacting us!");
+
+                setName('');
+                setSubject('');
+                setEmail('');
+                setPhone('');
+                setMessage('');
+            }
+            else {
+                alert("Something went Wrong")
+            }
+        }
+    };
+
     return (
         <div>
             <section id="section-wrapper">
-                <div class="box-wrapper">
-                    <div class="info-wrap">
-                        <h2 class="info-title">Contact Information</h2>
-                        <h3 class="info-sub-title">Fill up the form and our Team will get back to you within 24 hours</h3>
-                        <ul class="info-details">
+                <div className="box-wrapper">
+                    <div className="info-wrap">
+                        <h2 className="info-title">Contact Information</h2>
+                        <h3 className="info-sub-title">Fill up the form and our Team will get back to you within 24 hours</h3>
+                        <ul className="info-details">
                             <li>
-                                <i class="fas fa-phone-alt"></i>
+                                <i className="fas fa-phone-alt"></i>
                                 <span>Phone:</span> <a href="tel:+ 1235 2355 98">+ 1235 2355 98</a>
                             </li>
                             <li>
-                                <i class="fas fa-paper-plane"></i>
+                                <i className="fas fa-paper-plane"></i>
                                 <span>Email:</span> <a href="mailto:info@yoursite.com">info@gmail.com</a>
                             </li>
                             <li>
-                                <i class="fas fa-globe"></i>
+                                <i className="fas fa-globe"></i>
                                 <span>Website:</span> <a href="#">yoursite.com</a>
                             </li>
                         </ul>
-                        <ul class="social-icons">
-                            <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                        <ul className="social-icons">
+                            <li><a href="#"><i className="fab fa-facebook"></i></a></li>
+                            <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+                            <li><a href="#"><i className="fab fa-linkedin-in"></i></a></li>
                         </ul>
                     </div>
-                    <div class="form-wrap">
-                        <form action="#" method="POST">
-                            <h2 class="form-title">Send us a message</h2>
-                            <div class="form-fields">
-                                <div class="form-group">
-                                    <input type="text" class="fname" placeholder="First Name"/>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" class="lname" placeholder="Last Name"/>
-                                </div>
-                                <div class="form-group">
-                                    <input type="email" class="email" placeholder="Mail"/>
-                                </div>
-                                <div class="form-group">
-                                    <input type="number" class="phone" placeholder="Phone"/>
-                                </div>
-                                <div class="form-group">
-                                    <textarea name="message" id="" placeholder="Write your message"></textarea>
-                                </div>
+                    <div className="form-wrap">
+                        {/* <form action="#" method="POST"> */}
+                        <h2 className="form-title">Send us a message</h2>
+                        <div className="form-fields">
+                            <div className="form-group">
+                                <input type="text" className="fname" value={name} onChange={(e) => setName(e.target.value)} placeholder=" Name" required />
                             </div>
-                            <input type="submit" value="Send Message" class="submit-button"/>
-                        </form>
+                            <div className="form-group">
+                                <input type="text" className="lname" value={subject} onChange={(e) => setSubject(e.target.value)} placeholder="Subject" required />
+                            </div>
+                            <div className="form-group">
+                                <input type="email" className="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Mail" required />
+                            </div>
+                            <div className="form-group">
+                                <input type="number" className="phone" value={phone} onChange={(e) => setPhone(e.target.value)} placeholder="Phone" required />
+                            </div>
+                            <div className="form-group">
+                                <textarea name="message" id="" placeholder="Write your message" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+                            </div>
+                        </div>
+                        <input type="submit" value="Send Message" className="submit-button" onClick={handleClick} />
+                        {/* </form> */}
                     </div>
                 </div>
             </section>
